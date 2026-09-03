1f7fdb7c7d92172108a8187caf061169e0757197
Giray-Backend <guray@oniondevelopment.com.tr>
2026-09-03 15:18:43 +0300

    Initialize database tables on startup

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+import utils.db as db
+
 # Ortam değişkenlerini yükle
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
